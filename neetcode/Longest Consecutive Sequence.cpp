be9e3ffad6b63814344e5846f665c7ca0ef64265
Josef Knotek <adolf7890@gmail.com>
2026-03-08 08:55:39 +0100

    Create Longest Consecutive Sequence.cpp
    Given an array of integers nums, return the length of the longest consecutive sequence of elements that can be formed.
    
    A consecutive sequence is a sequence of elements in which each element is exactly 1 greater than the previous element. The elements do not have to be consecutive in the original array.
    
    You must write an algorithm that runs in O(n) time.
    
    Example 1:
    
    Input: nums = [2,20,4,10,3,4,5]
    
    Output: 4
    
    Explanation: The longest consecutive sequence is [2, 3, 4, 5].
    
    Example 2:
    
    Input: nums = [0,3,2,5,4,6,1,1]
    
    Output: 7
    
    Constraints:
    
        0 <= nums.length <= 1000
        -10^9 <= nums[i] <= 10^9

---
+++ neetcode/Longest Consecutive Sequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if(nums.size() == false){
+            return 0;
+        }
+
+        sort(nums.begin(),nums.end());
+        int longest = 1;
+        int current = 1;
+        for(int i = 1; i < nums.size();i++){
+            if(nums[i-1] != nums[i]){
+                if(nums[i] == nums[i-1]+1){
+                    current++;
+                }else{
+                    longest = max(longest,current);
+                    current = 1;
+                }
+            }
+        }
+        return max(longest,current);;
+        
+    }
+};
